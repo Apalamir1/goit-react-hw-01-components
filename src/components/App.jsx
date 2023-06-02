444afc9a9f24ead23bcaa30a4136a48507e40603
OleksandrZaitsev <apalamir17@gmail.com>
2023-06-02 15:48:06 +0300

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
-export const App = () => {
+import ProfileList from './Profile/ProfileList.js';
+import StatisticsList from './Statistics/StatisticsList.js';
+import FriendsList from './Friends/FriendsList.js';
+import TransactionHistory from './Transactions/TrancsactionHistory.js';
+
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <ProfileList user={user} />
+      <StatisticsList title="Upload stats" statistics={data} />
+      <FriendsList friends={friends} />
+      <TransactionHistory transaction={transactions} />
     </div>
   );
-};
+}
---
+++ src/components/Statistics/StatisticsList.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import Statistics from './Statistics';
+import style from './Statistics.module.css';
+
+export default function StatisticsList({ statistics, title }) {
+  return (
+    <section className={style.statistics}>
+      {title && <h2 className={style.title}>{title.toUpperCase()}</h2>}
+      <ul className={style.list}>
+        {statistics.map(({ id, label, percentage }) => (
+          <Statistics key={id} label={label} percentage={percentage} />
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+StatisticsList.propTypes = {
+  title: PropTypes.string.isRequired,
+  statistics: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      label: PropTypes.string,
+      percentage: PropTypes.number,
+    }).isRequired
+  ),
+};
